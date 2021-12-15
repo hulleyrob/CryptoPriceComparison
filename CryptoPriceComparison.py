657ff122ea7d316e14352e452639bdb04a924ede
Rob Hulley <hulleyrob@gmail.com>
2021-12-15 17:45:48 +0000

    changed MinMaxScaler to MaxAbsScaler

--- CryptoPriceComparison.py
+++ CryptoPriceComparison.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 
 from datetime import datetime, timedelta
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler,MaxAbsScaler
 import pandas as pd
 import pandas_datareader as pdr
 import plotly.graph_objects as go
 
 CRYPTOS = ['BTC', 'ETH', 'ADA', 'ANKR', 'DOT1','SOL1','HEX','BNB','DOGE','LUNA1','SHIB','XLM','LINK']
+# CRYPTOS = ['BTC', 'ETH', 'ADA']
 CURRENCY = 'USD'
 
 def getData(cryptocurrency):
     now = datetime.now()
     current_date = now.strftime("%Y-%m-%d")
-    last_year_date = (now - timedelta(days=1460)).strftime("%Y-%m-%d")
+    last_year_date = (now - timedelta(days=100)).strftime("%Y-%m-%d")
     start = pd.to_datetime(last_year_date)
     end = pd.to_datetime(current_date)
     data = pdr.get_data_yahoo(f'{cryptocurrency}-{CURRENCY}', start, end)
     print(f"{data=}")
-    data = pd.DataFrame(MinMaxScaler().fit_transform(data), columns=data.columns)
+    data = pd.DataFrame(MaxAbsScaler().fit_transform(data), columns=data.columns)
     return data
 crypto_data = {crypto:getData(crypto) for crypto in CRYPTOS}
 fig = go.Figure()
@@ -25,13 +26,13 @@ for idx, name in enumerate(crypto_data):
     fig = fig.add_trace(
         go.Scatter(
             x = crypto_data[name].index,
-            y = crypto_data[name].Close,
+            y = crypto_data[name].Close * 100,
             name = name,
         ))
 fig.update_layout(
     title = 'The Correlation between Different Cryptocurrencies',
-    xaxis_title = 'Date',
-    yaxis_title = f'Closing price (%)',
+    xaxis_title = 'Days',
+    yaxis_title = f'Closing price per day as perentage of Max price (%)',
     legend_title = 'Cryptocurrencies')
-fig.update_yaxes(type='linear', tickprefix='')
+fig.update_yaxes(type='linear', tickprefix='',ticksuffix='%')
 fig.show()
